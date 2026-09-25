27f2075481e9995f30fd1b032d97b70d017199ac
Emerson Nobre <emerson_14@live.com>
2026-09-24 21:02:52 -0400

    fix: executar Gradle no workflow de release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,8 +70,8 @@ android {
         applicationId = "com.ntv2.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.4.1"
 
         buildConfigField("int", "TELEGRAM_API_ID", telegramApiId.toString())
         buildConfigField("String", "TELEGRAM_API_HASH", "\"$telegramApiHash\"")

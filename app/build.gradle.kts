5ca89a2bf8a079653340803f260548d66229f26d
Emerson Nobre <emerson_14@live.com>
2026-09-24 23:51:53 -0400

    chore(release): 0.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,8 +70,8 @@ android {
         applicationId = "com.ntv2.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.4.4"
+        versionCode = 10
+        versionName = "0.4.5"
 
         buildConfigField("int", "TELEGRAM_API_ID", telegramApiId.toString())
         buildConfigField("String", "TELEGRAM_API_HASH", "\"$telegramApiHash\"")
